0d7547bde692cae045f5ffe7852bef7a2f5b7c4a
nileshthorave42-crypto <nileshthorave42@gmail.com>
2026-01-04 20:03:42 +0530

    Add files via upload

---
+++ Odd_Even Number.py
@@ -0,0 +1,6 @@
+Num = int(input("Enter a number here: "))
+
+if Num % 2 == 0:
+    print ("The entered Number", Num, "is even")
+else:
+    print ("The entered Number", Num, "is odd")
---
+++ Sum of Int 50.py
@@ -0,0 +1,7 @@
+total = 0
+for i in range(1,51):
+    total = total + i
+print(total)
+print(f"The sum of the numbers from 1 to 50 is: {total}")
+
+#total += i
